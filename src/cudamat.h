412cfabd7a0e92cc09e817a183c3d8376d7558cd
DDoyle34 <darcy.a.doyle@gmail.com>
2025-11-03 22:13:50 +0000

    Added header file which defines the bulk of functions that need to be implemented.

---
+++ src/cudamat.h
@@ -0,0 +1,36 @@
+#ifndef CUDAMAT_H
+#define CUDAMAT_H
+
+typedef struct {
+    float** el;
+    unsigned m;
+    unsigned n;
+} Matrix;
+
+Matrix init_const(unsigned m, unsigned n, float k);
+
+Matrix init_zero(unsigned m, unsigned n);
+
+Matrix init_ones(unsigned m, unsigned n);
+
+Matrix init_rand(unsigned m, unsigned n);
+
+void mfree(Matrix a);
+
+Matrix madd(Matrix a, Matrix b);
+
+Matrix mmult(Matrix a, Matrix b);
+
+Matrix kmult(float k, Matrix a);
+
+Matrix mtrans(Matrix a);
+
+Matrix msigmoid(Matrix a);
+
+Matrix ident(unsigned m);
+
+void mprint(Matrix a);
+
+Matrix ginvert(Matrix a);
+
+#endif
